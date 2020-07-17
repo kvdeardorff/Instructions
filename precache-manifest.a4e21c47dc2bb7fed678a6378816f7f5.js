78b8e5973cd0e5e5e47f9f558e84588fad7e7711
Kat Deardorff <kvdeardorff@gmail.com>
2020-07-16 21:49:41 -0700

    Updates

---
+++ precache-manifest.a4e21c47dc2bb7fed678a6378816f7f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf1b534306a6231de5b00f5df37c488",
+    "url": "/Instructions/index.html"
+  },
+  {
+    "revision": "85f6ef936f53b8f83646",
+    "url": "/Instructions/static/css/main.c5d8ebdf.chunk.css"
+  },
+  {
+    "revision": "bc82115053852d653131",
+    "url": "/Instructions/static/js/2.2952a1d5.chunk.js"
+  },
+  {
+    "revision": "f0ab5801985173a52868f196793396c8",
+    "url": "/Instructions/static/js/2.2952a1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85f6ef936f53b8f83646",
+    "url": "/Instructions/static/js/main.4c749b2b.chunk.js"
+  },
+  {
+    "revision": "24ea8490ee22c29c10d5",
+    "url": "/Instructions/static/js/runtime-main.2fa3c24d.js"
+  }
+]);
